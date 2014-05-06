13a3e1b28c005ea1b0e3e6204d8cc8bf45fe4c92
virtuozzo <virtuozz.me@gmail.com>
2014-05-07 00:43:52 +0400

    fix ILinkTable trait, add LinkTable classes

--- src/main/scala/mephi/oop/ILinkTable.scala
+++ src/main/scala/mephi/oop/ILinkTable.scala
@@ -1,61 +1,76 @@
 package mephi.oop
 
-/**
- * Таблица связей между объектами
- *
- * @tparam T Тип связей, хранящихся в таблице
- */
+import scala.collection.mutable
+
 trait ILinkTable[T <: ILink] {
 
-/**
- * Добавление связи
- *
- * @param link Добавляемая связь
- */
-def addLink(link: T)
+  val storage: mutable.HashMap[Int, mutable.HashMap[Int,T]] = new mutable.HashMap[Int, mutable.HashMap[Int,T]]
 
-/**
- * Добавление связи
- *
- * @param sourceId Идентификатор первого объекта (источника связи)
- * @param targetId Идентификатор второго объекта
- */
-def addLink(sourceId: Int, targetId: Int)
-
-/**
- * Удаление связи
- *
- * @param sourceId Идентификатор первого объекта (источника связи)
- * @param targetId Идентификатор второго объекта
- */
-def deleteLink(sourceId: Int, targetId: Int)
-
-/**
- * Получение списка идентификаторов объектов, связанных с указанным источником
- *
- * @param sourceId Идентификатор источника
- * @return Список идентификаторов
- */
-def getTargetIds(sourceId: Int): List[Integer]
-
-/**
- * Получение идентификатора источника
- * по идентификатору одного из
- * связанных с ним объектов
- *
- * @param targetId Идентификатор связанного объекта
- * @return Идентификатор источника
- */
-def getSourceId(targetId: Int): Int
-
-/**
- * Получение объекта-связи
- *
- * @param sourceId Идентификатор первого объекта (источника связи)
- * @param targetId Идентификатор второго объекта
- * @return Объект связи
- */
-def getLink(sourceId: Int, targetId: Int): T
+  var nextId: Int = 0
 
 
+  /**
+   * Получение объекта-связи
+   *
+   * @param sourceId Идентификатор первого объекта (источника связи)
+   * @param targetId Идентификатор второго объекта
+   * @return Объект связи
+   */
+  def getLink(sourceId: Int, targetId: Int): Option[T] = storage.get(sourceId) match {
+    case Some(parent: mutable.HashMap) => parent.get(targetId)
+    case _ => None
+  }
+
+  /**
+   * Получение идентификатора источника
+   * по идентификатору одного из
+   * связанных с ним объектов
+   *
+   * @param targetId Идентификатор связанного объекта
+   * @return Идентификатор источника
+   */
+  def getSourceId(targetId: Int): Int = {
+    val result: List[Int] = for(parent <- storage; child: ILink <- parent if child.getTargetId == targetId)
+    yield child.getSourceId()
+    result.head
+  }
+
+  /**
+   * Получение списка идентификаторов объектов, связанных с указанным источником
+   *
+   * @param sourceId Идентификатор источника
+   * @return Список идентификаторов
+   */
+  def getTargetIds(sourceId: Int): List[Int] = storage.get(sourceId) match {
+    case Some(parent: mutable.HashMap) => parent.values.map(x => x.getTargetId).toList
+    case _ => List()
+  }
+
+  /**
+   * Удаление связи
+   *
+   * @param sourceId Идентификатор первого объекта (источника связи)
+   * @param targetId Идентификатор второго объекта
+   */
+  def deleteLink(sourceId: Int, targetId: Int): Unit = storage.get(sourceId) match {
+    case Some(parent: mutable.HashMap) => parent.remove(targetId)
+    case _ =>
+  }
+
+  /**
+   * Добавление связи
+   *
+   * @param sourceId Идентификатор первого объекта (источника связи)
+   * @param targetId Идентификатор второго объекта
+   */
+  def addLink(sourceId: Int, targetId: Int): Unit
+
+
+
+  /**
+   * Добавление связи
+   *
+   * @param link Добавляемая связь
+   */
+  def addLink(link: T): Unit
 }
---
+++ src/main/scala/mephi/oop/models/LinkTables.scala
@@ -0,0 +1,65 @@
+package mephi.oop.models
+
+import mephi.oop.{ILinkTable}
+import scala.collection.mutable
+
+class DoctorPatientsTable extends ILinkTable[DoctorPatients]{
+  /**
+   * Добавление связи
+   *
+   * @param link Добавляемая связь
+   */
+  override def addLink(link: DoctorPatients): Unit = storage.get(link.source) match {
+    case Some(parent: mutable.HashMap) => parent += (link.source -> link)
+    case _ =>
+      val entry = new mutable.HashMap[Int, DoctorPatients]()
+      entry += link.source -> link
+      storage += link.source -> entry
+  }
+
+  /**
+   * Добавление связи
+   *
+   * @param sourceId Идентификатор первого объекта (источника связи)
+   * @param targetId Идентификатор второго объекта
+   */
+  override def addLink(sourceId: Int, targetId: Int): Unit = storage.get(sourceId) match {
+    case Some(parent: mutable.HashMap) => parent += (sourceId -> DoctorPatients(sourceId, targetId))
+    case _ =>
+      val entry = new mutable.HashMap[Int, DoctorPatients]()
+      entry += sourceId -> DoctorPatients(sourceId, targetId)
+      storage += sourceId -> entry
+  }
+}
+
+
+class WardPatientsTable extends ILinkTable[WardPatients] {
+  /**
+   * Добавление связи
+   *
+   * @param link Добавляемая связь
+   */
+  override def addLink(link: WardPatients): Unit = storage.get(link.source) match {
+    case Some(parent: mutable.HashMap) => parent += (link.source -> link)
+    case _ =>
+      val entry = new mutable.HashMap[Int, WardPatients]()
+      entry += link.source -> link
+      storage += link.source -> entry
+  }
+
+  /**
+   * Добавление связи
+   *
+   * @param sourceId Идентификатор первого объекта (источника связи)
+   * @param targetId Идентификатор второго объекта
+   */
+  override def addLink(sourceId: Int, targetId: Int): Unit = storage.get(sourceId) match {
+    case Some(parent: mutable.HashMap) => parent += (sourceId -> WardPatients(sourceId, targetId))
+    case _ =>
+      val entry = new mutable.HashMap[Int, WardPatients]()
+      entry += sourceId -> WardPatients(sourceId, targetId)
+      storage += sourceId -> entry
+  }
+
+}
+
