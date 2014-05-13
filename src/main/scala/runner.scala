8ac69bd14b2cd839910d05b72205befded15deb6
virtuozzo <virtuozz.me@gmail.com>
2014-05-14 00:39:08 +0400

    * edit gui

--- src/main/scala/runner.scala
+++ src/main/scala/runner.scala
@@ -1,4 +1,5 @@
 
+import mephi.oop.models.{Doctor, DoctorTable}
 import scala.swing._
 import scala.swing.event.{TableRowsSelected, TableColumnsSelected, TableEvent}
 
@@ -9,11 +10,20 @@ object runner extends App {
   val headers = Array.tabulate(10) {"Col-" + _}.toSeq
   val rowData = Array.tabulate[Any](10, 10) {"" + _ + ":" + _}
 
+  val id1 = DoctorTable.add(Doctor("House", 54))
+  val id2 = DoctorTable.add(Doctor("Dorian", 28))
+
+  val doctorHeaders = List("ID", "ФИО", "Возраст")
+  val patientsHeaders = List("ID", "ФИО", "Возраст")
+  val wardHeaders = List("№ палаты", "№ здания")
+
+  val doctorData = DoctorTable.storage.toArray.map(x => Array(x._1, x._2.fio, x._2.age))
+
 
   val frame = new MainFrame {
     title = "DB Manager"
     menuBar = new MenuBar {
-      contents += new Menu("File") {
+      contents += new Menu("Файл") {
         contents += new MenuItem(Action("Open") {
           //openFile
         })
@@ -24,6 +34,29 @@ object runner extends App {
         contents += new MenuItem(Action("Exit") {
           sys.exit(0)
         })
+      }
+      contents += new Menu("Таблица") {
+        contents += new MenuItem(Action("Доктора") {
+          //DoctorTable
+
+        })
+        contents += new MenuItem(Action("Пациенты") {
+          //PatientTable
+
+        })
+        contents += new MenuItem(Action("Палаты") {
+          //WardTable
+
+        })
+        contents += new MenuItem(Action("Доктор-пациенты") {
+          //DoctorPatientsTable
+
+        })
+        contents += new MenuItem(Action("Палата-пациеты") {
+          //WardPatientsTable
+
+        })
+
       }
     }
     contents = mainSection
@@ -39,13 +72,27 @@ object runner extends App {
       contents += new Button("Add row")
       contents += new Button("Delete row")
     }
-    contents += leftSide
+    contents += leftSide(doctorData, doctorHeaders)
     contents += rightSide
 
   }
 
+  def leftSide(rowData:Array[Array[Any]], headers:Seq[String]) = new BoxPanel(Orientation.Vertical) {
+    val textFields = new BoxPanel(Orientation.Horizontal) {
+      contents += new BoxPanel(Orientation.Vertical) {
+        contents += new Label("ФИО")
+        contents += new TextField
+      }
+      contents += new BoxPanel(Orientation.Vertical) {
+        contents += new Label("Возраст")
+        contents += new TextField
+      }
+      contents += new BoxPanel(Orientation.Vertical) {
+        contents += new Label(" ")
+        contents += new Button("Добавить")
+      }
 
-  lazy val leftSide = new BoxPanel(Orientation.Vertical) {
+    }
     val table  =  new Table(rowData, headers) {
       selection.elementMode = Table.ElementMode.Cell
       //selection.intervalMode = Table.IntervalMode.Single
@@ -78,6 +125,7 @@ object runner extends App {
       case e => println("%s => %s" format(e.getClass.getSimpleName, e.toString))
     }
 
+    contents += textFields
     contents += new ScrollPane(table)
     contents += new ScrollPane(output)
 
@@ -89,6 +137,7 @@ object runner extends App {
       output.append("%s\n  Lead: %s, %s; Rows: %s; Columns: %s\n" format (msg, rowId, colId, rows, cols))
     }
   }
+
 
 
 }
