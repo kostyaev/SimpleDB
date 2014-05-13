f4e39b7c36c4f94c22c2fd1168218568ff11716c
virtuozzo <virtuozz.me@gmail.com>
2014-05-13 23:37:49 +0400

    + getMostPopulatedWard, + test case

--- src/main/scala/mephi/oop/Queries.scala
+++ src/main/scala/mephi/oop/Queries.scala
@@ -1,12 +1,25 @@
 package mephi.oop
 
-import mephi.oop.models.{DoctorTable, DoctorPatientsTable, Doctor}
+import mephi.oop.models._
+import scala.Some
+import mephi.oop.models.Doctor
 
 object Queries {
 
-//  Создать метод, вычисляющий,
-//  в какой палате больше всего пациентов указанного врача.
+  /**
+    * Создать метод, вычисляющий,
+    * в какой палате больше всего пациентов указанного врача.
+    */
 
-  //def query1(doctor: Doctor) = DoctorTable. DoctorPatientsTable.getTargetIds(doctor.)
+  def getAllWardsOfDoctor(d: Doctor) = DoctorTable.findByAttributes(d.fio, d.age) match {
+    case Some(id) => DoctorPatientsTable.getTargetIds(id).flatMap(x => WardPatientsTable.getSourceId(x))
+    case None => List()
+  }
+
+  def getMostPopulatedWard(d: Doctor) =
+    getAllWardsOfDoctor(d) match {
+     case List() => None
+     case nonEmptyList => WardTable.get(nonEmptyList.groupBy(x => x).map(t => (t._1, t._2.size)).maxBy(e => e._2)._1)
+    }
 
 }
---
+++ src/test/scala/mephi/oop/QuerySuite.scala
@@ -0,0 +1,59 @@
+package mephi.oop
+
+import org.scalatest.{Matchers, FlatSpec}
+import mephi.oop.models._
+import mephi.oop.models.Ward
+import mephi.oop.models.Patient
+import mephi.oop.models.DoctorPatients
+import mephi.oop.models.Doctor
+import mephi.oop.models.WardPatients
+
+class QuerySuite extends FlatSpec with Matchers {
+  "Query getMostPopulatedWard" should "return the most populated ward with the patients of some particular doctor" in {
+    val id1 = DoctorTable.add(Doctor("House", 54))
+    val id2 = DoctorTable.add(Doctor("Dorian", 28))
+
+    val wId1 = WardTable.add(Ward(1,1))
+    val wId2 = WardTable.add(Ward(1,2))
+    val wId3 = WardTable.add(Ward(3,2))
+
+
+    val pId1 = PatientTable.add(Patient("Kate", 24))
+    val pId2 = PatientTable.add(Patient("Ashley", 26))
+    val pId3 = PatientTable.add(Patient("James", 43))
+    val pId4 = PatientTable.add(Patient("John", 28))
+    val pId5 = PatientTable.add(Patient("Maria", 28))
+    val pId6 = PatientTable.add(Patient("Helen", 23))
+    val pId7 = PatientTable.add(Patient("Max", 38))
+    val pId8 = PatientTable.add(Patient("Haviland", 27))
+
+    DoctorPatientsTable.addLink(DoctorPatients(id1,pId1))
+    DoctorPatientsTable.addLink(DoctorPatients(id1,pId2))
+
+    DoctorPatientsTable.addLink(DoctorPatients(id1,pId3))
+    DoctorPatientsTable.addLink(DoctorPatients(id1,pId4))
+    DoctorPatientsTable.addLink(DoctorPatients(id1,pId5))
+
+
+    DoctorPatientsTable.addLink(DoctorPatients(id2,pId6))
+    DoctorPatientsTable.addLink(DoctorPatients(id2,pId7))
+    DoctorPatientsTable.addLink(DoctorPatients(id2,pId8))
+
+    WardPatientsTable.addLink(WardPatients(wId1, pId1))
+    WardPatientsTable.addLink(WardPatients(wId1, pId2))
+
+    WardPatientsTable.addLink(WardPatients(wId2, pId3))
+    WardPatientsTable.addLink(WardPatients(wId2, pId4))
+    WardPatientsTable.addLink(WardPatients(wId2, pId5))
+
+    WardPatientsTable.addLink(WardPatients(wId1, pId6))
+    WardPatientsTable.addLink(WardPatients(wId3, pId7))
+    WardPatientsTable.addLink(WardPatients(wId3, pId8))
+
+
+    Queries.getMostPopulatedWard(Doctor("House", 54)) should be (Some(Ward(1,2)))
+    Queries.getMostPopulatedWard(Doctor("Dorian", 28)) should be (Some(Ward(3,2)))
+
+  }
+
+}
